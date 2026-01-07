1ec5a7eaeadcd27bc2d408c83bbd6ffd99cb4378
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2026-01-07 17:01:19 +0100

    Added a new folder for general relativity exercises.

---
+++ general_relativity/main.py
@@ -0,0 +1,39 @@
+# Main exercises for practicing with sympy in the context of general relativity.
+# https://chatgpt.com/share/695e8302-bba0-800e-b62b-5c4070667073
+
+
+def exercise_1():
+    pass
+
+
+def exercise_2():
+    pass
+
+
+def exercise_3():
+    pass
+
+
+def exercise_4():
+    pass
+
+
+def exercise_5():
+    pass
+
+
+def exercise_6():
+    pass
+
+
+def main():
+    exercise_1()
+    exercise_2()
+    exercise_3()
+    exercise_4()
+    exercise_5()
+    exercise_6()
+
+
+if __name__ == "__main__":
+    main()
